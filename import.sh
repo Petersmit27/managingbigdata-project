a8a0a93ba28f3abff8839f7293eca10ad5e4cc17
Peter Smit <peter@elnino.tech>
2024-01-09 12:34:17 +0100

    na dinsdag

---
+++ import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for year in $(seq 2018 2022);
+do
+    echo "---------- Starting $year ----------"
+    for month in $(seq -f "%02g" 1 12);
+    do
+        echo "Downloading clickstream-enwiki-$year-$month.tsv.gz..."
+        wget https://dumps.wikimedia.org/other/clickstream/$year-$month/clickstream-enwiki-$year-$month.tsv.gz
+        echo "Moving clickstream-enwiki-$year-$month.tsv.gz to DFS..."
+        hdfs dfs -copyFromLocal ./clickstream-enwiki-$year-$month.tsv.gz ./clickstream/
+        rm clickstream-enwiki-$year-$month.tsv.gz
+    done
+    echo "---------- Done with $year ----------"
+done
+
+echo "Done!"
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, lower, desc, col, collect_set
+from pyspark import SparkContext
 
+sc = SparkContext(appName="Clickstream project group 20")
 spark = SparkSession.builder.getOrCreate()
 
+from pyspark.sql.functions import explode, lower, desc, col, collect_set, input_file_name, collect_list, sort_array, expr, slice, struct, row_number
+from pyspark.sql.window import Window
 from pyspark.sql.types import StringType, IntegerType, StructType, StructField
 
 schema = StructType([
@@ -12,10 +15,69 @@ schema = StructType([
   StructField("count", IntegerType()),
 ])
 
-tweets = spark.read \
-    .option("sep", "\t") \
-    .schema(schema) \
-    .csv('/user/s2030918/click_oktober.tsv')
+clickstreamData = [
+    spark.read \
+        .option("sep", "\t") \
+        .schema(schema) \
+        .csv(f'/user/s1934538/clickstream/clickstream-enwiki-{year}-{month:02d}.tsv.gz') \
+            for year in range(2023, 2024) \
+            for month in range(10, 13)
+]
+
+# ------------ STAGE 1: Collecting top 10 most popular pages each month ------------
+popularPages = set()
+
+motherframe = spark.createDataFrame([], schema=StructType([
+    StructField("from", StringType()),
+    StructField("to", StringType()),
+    StructField("type", StringType()),
+    StructField("count", IntegerType()),
+    StructField("file", StringType()),
+]))
+
+for monthData in clickstreamData:
+    motherframe = motherframe.union(monthData.withColumn('file', input_file_name()))
+
+    # toClicks = monthData \
+    #     .groupBy('to') \
+    #     .sum('count') \
+    #     .select(col('to').alias('url'), col('sum(count)').alias('toClicks')) \
+    #     .sort('toClicks', ascending=False) \
+    #     .limit(10) \
+    #     .select('url').rdd.flatMap(lambda x: x).collect() # https://stackoverflow.com/questions/38610559/convert-spark-dataframe-column-to-python-list
+    
+    # popularPages.update(set(toClicks))
+
+motherframe.show()
+motherframe.printSchema()
+
+
+windowDept = Window.partitionBy("url").orderBy(col("toClicks").desc())
+df2=df.withColumn("row",row_number().over(windowDept))
+
+popularPages = motherframe \
+    .groupBy(['file', 'to']) \
+    .sum('count') \
+    .select('file', col('to').alias('url'), col('sum(count)').alias('toClicks')) \
+    \
+    .withColumn('rank', row_number().over(Window.partitionBy('file', "url").orderBy(col("toClicks").desc()))) \
+    .filter('rank <= 10') \
+    .drop('rank')
+    # .groupBy('file').agg(slice(sort_array(collect_list(struct('toClicks', 'url')), asc=False),1,10)) \
+    # .sort('toClicks', ascending=False) \
+    # .limit(10) \
+    .select('file', 'url').collect() # https://stackoverflow.com/questions/38610559/convert-spark-dataframe-column-to-python-list
+
+print(popularPages)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,4 +93,8 @@ fromClicks =  tweets \
 
 clicks = toClicks.join(fromClicks, 'url')
 
-clicks.withColumn('ratio', col('toClicks') / col('fromClicks'))
+clicks3 = clicks \
+    .where('toClicks > 10000 and fromClicks > 10000') \
+    .withColumn('ratio', col('toClicks') / col('fromClicks')) \
+    .sort('ratio', ascending = False)
+
